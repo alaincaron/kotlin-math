03bdc6f5ec8c9ff356c309657a4474d0fb811cce
Alain Caron <caron.alain@gmail.com>
2023-02-07 12:50:56 -0500

    Fixed parsing of pure imaginary numbers

--- lib/src/main/kotlin/org/alc/math/complex/ComplexIO.kt
+++ lib/src/main/kotlin/org/alc/math/complex/ComplexIO.kt
@@ -4,9 +4,10 @@ import java.io.DataInput
 import java.io.DataOutput
 import java.util.regex.Pattern
 
-internal const val doubleRegex = "(?:[-+]?(?:[0-9]*[.])?[0-9]+(?:e[-+]?[0-9]+)?)"
+internal const val signRegex = "[+-]?"
+internal const val doubleRegex = "(?:(?:[0-9]*[.])?[0-9]+(?:e${signRegex}[0-9]+)?)"
 internal val complexRegex = Pattern.compile(
-    "^($doubleRegex)(i?)|($doubleRegex)\\s*([+-])\\s*($doubleRegex)i$",
+    "($signRegex)(?:($doubleRegex?)(i?)|($doubleRegex)\\s*([+-])\\s*($doubleRegex?)i)$",
     Pattern.CASE_INSENSITIVE
 )
 
@@ -20,17 +21,18 @@ fun String.toComplex(): Complex {
     if (this == "NaN") return Complex.NaN
     val m = complexRegex.matcher(this)
     if (!m.matches()) throw NumberFormatException("Invalid complex number: \"$this\"")
-    if (m.group(1) != null) {
-        val v = m.group(1).toDouble()
-        return if (m.group(2).isEmpty()) Complex(v, 0.0) else Complex(0.0, v)
+    val sign = if (m.group(1) == "-") -1.0 else 1.0
+    if (m.group(2) != null) {
+        if (m.group(2).isEmpty() && m.group(3).isEmpty())
+            throw NumberFormatException("Invalid complex number: (empty string)")
+        val v = if (m.group(2).isEmpty()) sign else m.group(2).toDouble() * sign
+        return if (m.group(3).isEmpty()) Complex(v, 0.0) else Complex(0.0, v)
     }
-    val re = m.group(3).toDouble()
-    if (m.group(5).startsWith("-")) throw NumberFormatException("Invalid complex number: \"$this\"")
-    val im = m.group(5).toDouble()
-    return if (m.group(4) == "+")
-        Complex(re, im)
-    else
-        Complex(re, -im)
+    val re = m.group(4).toDouble() * sign
+    val sign2 = if (m.group(5) == "-") -1.0 else 1.0
+
+    val im = if (m.group(6).isEmpty()) sign2 else m.group(6).toDouble() * sign2
+    return Complex(re, im)
 }
 
 fun DataInput.readComplex(): Complex {
